39c3512db3ac379125d2bddb15dd448440659a44
Thunderofnl <tunderofnl@gmail.com>
2015-05-19 11:11:45 +0200

    Add a default alias if the alias is blank

--- core/components/setinputoptions/processors/mgr/inputoption/create.class.php
+++ core/components/setinputoptions/processors/mgr/inputoption/create.class.php
@@ -15,6 +15,11 @@ class SetInputOptionsInputOptionCreateProcessor extends modObjectCreateProcessor
     {
         $items = $this->modx->getCollection($this->classKey, array('group' => $this->getProperty('group'), 'softDelete' => false));
 
+        //Set a default alias
+        if (!$this->getProperty('alias')) {
+            $this->setProperty('alias', strtolower(str_replace(" ", "-", $this->getProperty('name'))));
+        }
+
         $this->setProperty('position', count($items));
 
         return parent::beforeSet();
